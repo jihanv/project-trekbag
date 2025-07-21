d2397d41ceda6cd92f0a0ce1b25699bdcf3b4ace
jihanv <hedjmny@gmail.com>
2025-07-21 15:10:59 +0900

    Add item list

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,3 +1,25 @@
+import { useState } from "react";
+import { initalItems } from "../lib/constants";
+
 export default function ItemList() {
-  return <div>ItemList</div>;
+  const [items, setItems] = useState(initalItems);
+  return (
+    <ul>
+      {items.map((item) => (
+        <Item key={item.id} item={item} />
+      ))}
+    </ul>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li className="item">
+      <label>
+        <input type="checkbox" checked={item.packed} />
+        {item.name}
+      </label>
+      <button>❌</button>
+    </li>
+  );
 }
